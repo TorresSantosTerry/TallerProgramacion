53b61f156f0c048bfcf03b6bb173dbc45dab6747
Terry <Terry@Terry>
2024-08-13 20:42:32 -0500

    Commit2

---
+++ Proyect1/src/proyect1/Proyect1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyect1;
+
+/**
+ *
+ * @author Terry
+ */
+public class Proyect1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola Mundo");
+        
+        System.out.println("Hola mundo 2");
+    }
+    
+}
